617fc9b975420eb05bcab908d6787fe53a7b6172
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-10-29 11:44:40 -0700

    got basic intro text and functionality, needs new styles

--- client/app/introText/introText.directive.js
+++ client/app/introText/introText.directive.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('triviziApp')
-    .directive('introText', function() {
+    .directive('introText', function () {
         return {
             templateUrl: 'app/introText/introText.html',
             restrict: 'EA',
-            link: function(scope, element, attrs) {}
+            link: function ($scope, $element, $attrs, ean) {
+                $scope.introSubmit = function () {
+                    console.log('seekdeer');
+                    $("intro-text").remove();
+                    $("full-text").show();
+                    $scope.seekDeer();
+                }
+            }
         };
     });
