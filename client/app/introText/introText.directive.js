f8262c8b582d8c14aac3244b8f12219a15015881
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-10-29 15:47:34 -0700

    added second auto complete intput to intro text

--- client/app/introText/introText.directive.js
+++ client/app/introText/introText.directive.js
@@ -5,11 +5,27 @@ angular.module('triviziApp')
         return {
             templateUrl: 'app/introText/introText.html',
             restrict: 'EA',
+            controller: function ($scope, $element, $attrs, ean) {
+
+                var options = {
+                        types: ['geocode']
+                    },
+                    navIntroInput = document.getElementById('navIntroLocationField'),
+                    navIntroAutocomplete = new google.maps.places.Autocomplete(navIntroInput, options);
+
+                $scope.navIntroLocationChanged = google.maps.event.addListener(navIntroAutocomplete, 'place_changed', function (e) {
+                    $scope.destination = navIntroAutocomplete.getPlace();
+                    $scope.specificLocation = $scope.destination.formatted_address;
+                    $scope.seekDeer($scope.destination.formatted_address);
+                    console.log('intro ran');
+                });
+
+            },
             link: function ($scope, $element, $attrs, ean) {
+
                 $scope.introSubmit = function () {
-                    console.log('seekdeer');
-                    $("intro-text").remove();
-                    $("full-text").show();
+                    //$("intro-text").remove();
+                    //$("#mainView").append("<full-text></full-text>");
                     $scope.seekDeer();
                 }
             }
