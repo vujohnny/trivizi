ec5dd6c99f1fb04899b848fd3f628c3a71c2e73a
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-10-29 22:53:06 -0700

    remove calendar controls from main

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,32 +3,6 @@
 
     function MainController($scope, $http, socket, $filter, ean, yelp) {
 
-        $scope.arriveDate = {
-            defaultValue: new Date(),
-            minDate: new Date() - 1,
-            maxDate: new Date().setFullYear(new Date().getFullYear() + 2),
-            showweeks: false,
-            mode: "month"
-        }
-
-        $scope.departDate = {
-            defaultValue: new Date(),
-            minDate: new Date() - 1,
-            maxDate: new Date().setFullYear(new Date().getFullYear() + 2),
-            showweeks: false,
-            mode: "month"
-        };
-
-        $scope.$watch("arriveDate.defaultValue", function() {
-            $scope.calendarArrive = $filter('date')($scope.arriveDate.defaultValue, 'MM/dd/yyyy');
-            //console.log("From: " + $scope.calendarArrive);
-        });
-
-        $scope.$watch("departDate.defaultValue", function() {
-            $scope.calendarDepart = $filter('date')($scope.departDate.defaultValue, 'MM/dd/yyyy');
-            //console.log("To: " + $scope.calendarDepart);
-        });
-
         $scope.seekDeer = function() {
             $http.post('/api/things', {
                 name: "$" + $scope.budgetAmount + " | " + $scope.calendarArrive + " - " + $scope.calendarDepart + " | " + $scope.specificLocation
