3707ea7d2791cddebb49dde7b06ee28639460d10
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-11-05 13:42:17 -0800

    added better check logic for seekdeer submit

--- client/app/filters/dateDeparture/dateDeparture.directive.js
+++ client/app/filters/dateDeparture/dateDeparture.directive.js
@@ -12,7 +12,7 @@ angular.module('triviziApp')
 
                 $scope.departDate = {
                     defaultValue: defaultDate.setDate(defaultDate.getDate() + defaultDateDaysToAdd),
-                    minDate: new Date() - 1,
+                    //minDate: new Date() - 1,
                     maxDate: new Date().setFullYear(new Date().getFullYear() + 2),
                     showweeks: false,
                     mode: "month"
@@ -20,8 +20,15 @@ angular.module('triviziApp')
 
                 $scope.departureChange = function () {
                     $scope.calendarDepart = $filter('date')($scope.departDate.defaultValue, 'MM/dd/yyyy');
-                    //$scope.seekDeer();
+                    $scope.seekDeer();
                 }
+                
+                $scope.calendarDepart = $filter('date')($scope.departDate.defaultValue, 'MM/dd/yyyy');
+                
+                /*
+                    $scope.$watch("arriveDate.defaultValue", function () {
+                    });
+                */
                 
             },
             link: function ($scope, $element, $attrs, $filter) {}
