817c148c7ccddaeb3323a45b13927548c31c7693
Johnny Vu <jvu@guthy-renker.com>
2015-11-15 14:47:40 -0800

    fix category issue on production refactory all category code api change route socket

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ import path from 'path';
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/category', require('./api/category'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
