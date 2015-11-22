8c0402e7c25551d0a71259a5c9ec1410841c7b86
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-11-22 12:03:18 -0800

    ready for build without marker windows or pink map

--- client/app/googleMap/googleMap.directive.js
+++ client/app/googleMap/googleMap.directive.js
@@ -127,9 +127,9 @@ angular.module('triviziApp')
                             $scope.markersDisplay(lat, lng);
                             google.maps.event.addListener(marker, 'click', (function (marker, i) {
                                 return function () {
-                                    //infowindow.setContent("<div id=\"" + id + "\" class=\"markerDisplay typography\"><span class=\"markerTotal\">$" + roundedTotal + "</span> <span class=\"medium-grey\" style=\"display: none;\">|</span> <img src=\"" + ratingImg + "\" class=\"ratingImg\" style=\"display: none;\"></div>");
-                                    infowindow.setContent("<div id=\""+id+"\" class=\"markerDisplay typography\"><span class=\"markerTotal\">$"+roundedTotal+"</span><img src=\"http://images.travelnow.com/"+listImg+"\" class=\"windowImg\"><span class=\"windowTitle\">"+name+"</span></div>");
-                                    infowindow.open(map, marker);
+                                    infowindow.setContent("<div id=\"" + id + "\" class=\"markerDisplay typography\"><span class=\"markerTotal\">$" + roundedTotal + "</span> <span class=\"medium-grey\" style=\"display: none;\">|</span> <img src=\"" + ratingImg + "\" class=\"ratingImg\" style=\"display: none;\"></div>");
+//                                    infowindow.setContent("<div id=\""+id+"\" class=\"markerDisplay typography\"><span class=\"markerTotal\">$"+roundedTotal+"</span><img src=\"http://images.travelnow.com/"+listImg+"\" class=\"windowImg\"><span class=\"windowTitle\">"+name+"</span></div>");
+//                                    infowindow.open(map, marker);
                                     $scope.highlightResult(id);
                                 }
                             })(marker, i));
